6a0a27a98458d0fe66f71813cd77b32dca95d5df
OjasaluS <siim.ojasalu@gmail.com>
2020-10-22 13:15:01 +0300

    task 1 fetching

--- res/js/script.js
+++ res/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-  $(".avatar").click(function(){
-    $(".dropdown-menu").toggle();
-  });
+    $(".avatar").click(function(){
+        $(".dropdown-menu").toggle();
+    });
+    $.get("https://private-anon-19b2331a1a-wad20postit.apiary-mock.com/users/1", function(data) {
+        $(".result" ).html(data);
+        let firstname = data.firstname;
+        let lastname = data.lastname;
+        let email = data.email;
+        let picture = data.avatar;
+        $(".name").text(firstname.concat(" ", lastname));
+        $(".email").text(email);
+        $(".avatar").attr("src", picture);
+    });
 });
