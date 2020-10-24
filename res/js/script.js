7a67f3cb7eea1d0c57f9139419ae480bb255d2e1
kasparkadalipp <kaspar.kadalipp@gmail.com>
2020-10-24 11:34:54 +0300

    Dynamically load posts data

--- res/js/script.js
+++ res/js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-    $(".avatar").click(function(){
+$(document).ready(function () {
+    $(".avatar").click(function () {
         $(".dropdown-menu").toggle();
     });
-    $.get("https://private-anon-19b2331a1a-wad20postit.apiary-mock.com/users/1", function(data) {
-        $(".result" ).html(data);
+    $.get("https://private-anon-19b2331a1a-wad20postit.apiary-mock.com/users/1", function (data) {
+        $(".result").html(data);
         let firstname = data.firstname;
         let lastname = data.lastname;
         let email = data.email;
@@ -12,4 +12,49 @@ $(document).ready(function(){
         $(".email").text(email);
         $(".avatar").attr("src", picture);
     });
+    $.get("https://private-anon-af03cd5f74-wad20postit.apiary-mock.com/posts",
+        data => data.forEach(content => $(".main-container").append(Post(content))));
 });
+
+const Post = ({author, createTime, media, text, likes}) => `
+    <div class="post">
+        <div class="post-author">
+            ${User(author)}
+            <small>${createTime}</small>
+        </div>
+        ${media !== null ? Media(media) : ''}
+        ${text !== null ? Text(text) : ''}
+        <div class="post-actions">
+            <button type="button" name="like" class="like-button">${likes}</button>
+        </div>
+    </div>
+`;
+
+const Text = (text) =>`
+    <div class="post-title">
+        <h3>${text}</h3>
+    </div>`
+
+const User = ({avatar, firstname, lastname}) => `
+    <span class="post-author-info">
+        <img src=${avatar} alt="Post author">
+        <small>${firstname + ' ' + lastname}</small>
+    </span>`
+
+const Media = ({type, url}) =>
+    type === 'video'
+        ?
+        `<div class="post-video">
+            <video controls>
+                <source src=${url} type=${'video/' + url.match(/[^.]+$/)[0]}>
+            </video>
+        </div>`
+        :
+        `<div class="post-image">
+            <img src=${url} alt="">
+        </div>`
+
+
+
+
+
